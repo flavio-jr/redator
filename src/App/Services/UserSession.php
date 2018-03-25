811cc0df634e13db17ca34894b8ef75127501d5f
Flavio Jr <flvribeiro27@gmail.com>
2018-03-25 11:05:57 +0000

    JWT token generation

--- src/App/Services/UserSession.php
+++ src/App/Services/UserSession.php
@@ -3,11 +3,25 @@
 namespace App\Services;
 
 use App\Entities\User;
+use Lcobucci\JWT\Builder;
 
 class UserSession
 {
+    const EXPIRATION_TIME = 3600;
+
     public function createNewToken(User $user): string
     {
-        return '';
+        $builder = new Builder();
+
+        $token = $builder
+            ->setIssuer('')
+            ->setAudience('')
+            ->setId(uniqid())
+            ->setIssuedAt(time())
+            ->setNotBefore(time())
+            ->setExpiration(time() + self::EXPIRATION_TIME)
+            ->getToken();
+        
+        return (string) $token;
     }
 }
