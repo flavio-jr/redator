ccb19162a6ac7b864cc9ee6736287b8bb1e26c16
Flavio Jr <flvribeiro27@gmail.com>
2018-03-26 08:15:33 +0000

    Added validation for Login request

--- src/App/routes.php
+++ src/App/routes.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Middlewares\LoggedUser;
+use App\RequestValidators\Login;
 
 $app->group('/app', function () {
-    $this->post('/login', 'App\Controllers\LoginController:login');
+    $this->post('/login', 'App\Controllers\LoginController:login')->add(new Login());
 
     $this->group('', function () {
         $this->get('', function ($request, $response) {
