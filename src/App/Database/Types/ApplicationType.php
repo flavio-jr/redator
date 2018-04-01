fd2e32fb65022a51814f99ab7a6f024802305fcd
Flavio Jr <flvribeiro27@gmail.com>
2018-04-01 16:48:12 +0000

    Database classes documentation

--- src/App/Database/Types/ApplicationType.php
+++ src/App/Database/Types/ApplicationType.php
@@ -4,10 +4,23 @@ namespace App\Database\Types;
 
 final class ApplicationType
 {
+    /**
+     * Newsletter type
+     * @var string
+     */
     public const NL = 'Newsletter';
-
+    
+    /**
+     * Landing page type
+     * @var string
+     */
     public const LP = 'Landing page';
 
+    /**
+     * Get all aplication types
+     * @method getApplicationTypes
+     * @return array
+     */
     public static function getApplicationTypes(): array
     {
         $reflection = new \ReflectionClass(__CLASS__);
