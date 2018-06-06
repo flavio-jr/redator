881ae5c6de3407497c8436a2d8dde7045719efb5
Flávio Jr <flavioribeirojr@outlook.com>
2018-06-06 20:15:22 +0000

    Command for creation of the superuser

--- src/App/Containers/CommandsContainer.php
+++ src/App/Containers/CommandsContainer.php
@@ -4,13 +4,14 @@ namespace App\Containers;
 
 use Slim\Container;
 use App\Commands\CreateMasterUser;
+use App\Repositories\UserMasterRepository\Store\UserMasterStore;
 
 class CommandsContainer
 {
     public function register(Container $container, array $config)
     {
         $container[CreateMasterUser::class] = function (Container $c) use ($config) {
-            return new CreateMasterUser();
+            return new CreateMasterUser($c->get(UserMasterStore::class));
         };
     }
 }
