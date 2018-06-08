7ad4bbfbbedd39fa83bb8bbaabb01bd6370c6e77
Flávio Jr <flavioribeirojr@outlook.com>
2018-06-08 13:36:39 +0000

    Command for update master user data

--- src/App/Containers/CommandsContainer.php
+++ src/App/Containers/CommandsContainer.php
@@ -5,13 +5,19 @@ namespace App\Containers;
 use Slim\Container;
 use App\Commands\CreateMasterUser;
 use App\Repositories\UserMasterRepository\Store\UserMasterStore;
+use App\Commands\UpdateMasterUser;
+use App\Repositories\UserMasterRepository\Update\UserMasterUpdate;
 
 class CommandsContainer
 {
     public function register(Container $container, array $config)
     {
-        $container[CreateMasterUser::class] = function (Container $c) use ($config) {
+        $container[CreateMasterUser::class] = function (Container $c) {
             return new CreateMasterUser($c->get(UserMasterStore::class));
+        };
+
+        $container[UpdateMasterUser::class] = function (Container $c) {
+            return new UpdateMasterUser($c->get(UserMasterUpdate::class));
         };
     }
 }
