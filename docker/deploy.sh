3d784cc0b3b9d1751066fa09e7ef53a2b8922b35
Flávio Jr <flavioribeirojr@outlook.com>
2018-06-29 18:44:24 +0000

    Refactors deploy script to use heroku api

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -10,9 +10,18 @@ case "$1" in
         APP_NAME=$APP_DEV_NAME
 esac
 
-cp $DIR/.netrc $HOME/.
-
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin registry.heroku.com
 docker build -t registry.heroku.com/${APP_NAME}/web $DIR/../.
 docker push registry.heroku.com/${APP_NAME}/web
-heroku container:release web -a $APP_NAME
+
+curl -n -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "'$(docker inspect registry.heroku.com/$APP_NAME/web --format={{.Id}})'"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
