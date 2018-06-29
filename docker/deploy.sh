4c76bb69ea9f219bfa272595891dc86395094f2c
Flávio Jr <flavioribeirojr@outlook.com>
2018-06-29 10:52:25 +0000

    Deletes docker data folder before deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -10,6 +10,8 @@ case "$1" in
         APP_NAME=$APP_DEV_NAME
 esac
 
+rm -rf docker/data
+
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin registry.heroku.com
 docker build -t registry.heroku.com/${APP_NAME}/web $DIR/../.
 docker push registry.heroku.com/${APP_NAME}/web
